0615c92491cbc41d796eaf9807893b28971b8478
Amit Sharma <amitxpr@gmail.com>
2020-04-25 15:29:22 +0530

    Updates

---
+++ precache-manifest.16d46dafc78fc4ea552d326a1a39659c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9d8634a7c0c8f5d9340b65840d4f654",
+    "url": "/myreactapp/index.html"
+  },
+  {
+    "revision": "31bc352807f61eba21d0",
+    "url": "/myreactapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5530b2c513b35509aa13",
+    "url": "/myreactapp/static/js/2.13f4d69f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myreactapp/static/js/2.13f4d69f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31bc352807f61eba21d0",
+    "url": "/myreactapp/static/js/main.f35eeff8.chunk.js"
+  },
+  {
+    "revision": "2b6bdc64e8da437c27ba",
+    "url": "/myreactapp/static/js/runtime-main.33a2426b.js"
+  },
+  {
+    "revision": "92305e7223b801b427b8a6a3b1c3ac44",
+    "url": "/myreactapp/static/media/pic01.92305e72.jpg"
+  },
+  {
+    "revision": "7a1cc4cec8edb4bfc754642868db0693",
+    "url": "/myreactapp/static/media/pic02.7a1cc4ce.jpg"
+  },
+  {
+    "revision": "28b540b353c1c2915ec58a845ac3f2d9",
+    "url": "/myreactapp/static/media/pic03.28b540b3.jpg"
+  },
+  {
+    "revision": "9aedd6f1b393a718bb1db19cdf3887aa",
+    "url": "/myreactapp/static/media/pic04.9aedd6f1.jpg"
+  },
+  {
+    "revision": "ee1864421c844f4af8305a3112d8b73f",
+    "url": "/myreactapp/static/media/pic05.ee186442.jpg"
+  },
+  {
+    "revision": "542ba962741138d27b795723c2f6f627",
+    "url": "/myreactapp/static/media/pic06.542ba962.jpg"
+  }
+]);
